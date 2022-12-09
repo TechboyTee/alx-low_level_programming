4b6eb6335dde28083b2b5ecc3286498bce933707
TechboyTee <abayomitayo200@gmail.com>
2022-12-08 21:05:05 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 main(void)
 int main(void)
 {
-	printf("Size of a char: %i bytes(s)\n", sizeof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a char: %ln bytes(s)\n", sizeof(char));
+	printf("Size of an int: %ln byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ln byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ln byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ln byte(s)\n", sizeof(float));
 	return (0);
 }
