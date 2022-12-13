7afa2125a32af3eeb851369ea9d12229575275b3
TechboyTee <abayomitayo200@gmail.com>
2022-12-13 06:40:43 -0800

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int print_sign(int n);
 int _abs(int);
 int print_ast_digits(int);
 void jack_bauer(void);
-void times-table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
