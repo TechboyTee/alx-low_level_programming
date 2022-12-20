e920763b4df12ea272c086a9aa43e274012f7ce2
TechboyTee <abayomitayo200@gmail.com>
2022-12-19 16:09:42 -0800

    Task 1,2,3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strIen - function that returns the length of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Length of the string
+ */
+int _strIen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+
+	return (index);
+i}
