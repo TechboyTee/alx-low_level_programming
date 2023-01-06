dd2c9ee37013b17e9d817c545c1b73ae9e504653
TechboyTee <abayomitayo200@gmail.com>
2023-01-05 18:09:29 -0800

    Task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,18 +2,20 @@
 
 /**
  * _strien_recursion - Return the length of a string.
- * @s: The string to be measured.
+ * @s: pointer  to string
  *
  * Return: The lenght of the string.
  */
-int _strien_recursion(char *s)
+int _strien_recursion(char s)
+
 {
-	if (*s)
-	{
-		return (1 + _strien_recursion(s + 1));
-	}
-	else
-	{
+	int c;
+
+	c = 0;
+	if (*s == '\0')
 		return (0);
-	}
+
+	c = 1 +  _strlen_recursion(s + 1);
+
+	return (c);
 }
